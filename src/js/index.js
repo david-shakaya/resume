aeb31fa41eeac02a354d7acb56a9edb7b79c3a20
david-shakaya <devidoz666@gmail.com>
2021-03-07 21:06:47 +0200

    Функц скрытия текста

--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,33 @@ const tabItem = document.querySelectorAll('.list-tab-item');
 listTabs.addEventListener('click', showItem);
 
 function showItem(e) {
-  if (e.target.nodeName !== 'BUTTON') {
-    return;
+  onClickСrossHideItem(e);
+
+  if (e.target.classList[0] === 'list-tab-item-btn') {
+    tabItem.forEach(el => {
+      if (el.classList[2]) {
+        el.classList.remove('active-item');
+        el.children[2].classList.remove('hide-tab-btn');
+      }
+    });
+    e.target.offsetParent.classList.add('active-item');
+    e.target.classList.add('hide-tab-btn');
   }
-  tabItem.forEach(el => {
-    if (el.classList[2]) {
-      el.classList.remove('active-item');
-      el.children[2].classList.remove('hide-tab-btn');
+}
+
+// eсли клик на крестик удаляем класы
+function onClickСrossHideItem(event) {
+  if (event.target.offsetParent.classList[2]) {
+    try {
+      if (event.target.previousElementSibling.nodeName === 'P') {
+        event.target.offsetParent.classList.remove('active-item');
+
+        tabItem.forEach(el => {
+          el.children[2].classList.remove('hide-tab-btn');
+        });
+      }
+    } catch {
+      (' error');
     }
-  });
-  e.target.offsetParent.classList.add('active-item');
-  e.target.classList.add('hide-tab-btn');
+  }
 }
